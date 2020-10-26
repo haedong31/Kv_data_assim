bcae7c953dcad3260fd9b7db1c889f605b45c041
Haedong Kim - Mac <haedong.kim90@gmail.com>
2020-10-25 23:35:00 -0400

    Pilot1 - Parameterization
    Total 28 parameters

--- scratch.m
+++ scratch.m
@@ -6,10 +6,13 @@ clear variables
 holding_p = -80; %mV
 holding_t = 10; %msec
 P1s = -70:10:50; %mV
-P1_t = 5000; % msec
+P1_t = 25000; % msec
 
 for i=1:length(P1s)
+    tic
     [t, ~, A, ~] = RasmussonUnparam(holding_p, holding_t, P1s(i), P1_t, P1s(i), P1_t);
+    toc
+    
     [t1, A1] = analytic_form(holding_p, holding_t, P1s(i), P1_t);
     
     figure(1)
@@ -48,3 +51,30 @@ for i=1:length(P1s)
     ylabel('Current (pA/pF)')
     axis tight
 end
+
+
+%% pilot 1
+clc
+close all
+clear variables
+
+holdV = -70; %mV
+holdT = 450; %ms
+P1 = 50; %mV
+sim_time = 25*1000; % ms
+N0 = 50;
+
+rng(7981);
+init_gen = unifrnd(0, 0.1, N0, 27);
+init_gen(:, 9) = unifrnd(0, 1, N0, 1);
+init_gen(:, 18) = unifrnd(0, 1, N0, 1);
+init_gen(:, 27) = unifrnd(0, 1, N0, 1);
+
+[t, A] = Ktrace1(init_gen(7,:), holdV, holdT, P1, sim_time);
+
+plot(t, A(:, 1))
+hold on
+    plot(t, A(:, 2))
+    plot(t, A(:, 3))
+hold off
+legend('Ito', 'IKslow1', 'IKslow2')
