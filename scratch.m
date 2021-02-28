8694c9e83a3b7558f186999deadf89e759b9dd03
Haedong Kim - Win <haedong.kim90@gmail.com>
2021-02-28 13:09:15 -0500

    MATLAB interactive plot

--- scratch.m
+++ scratch.m
@@ -1,3 +1,34 @@
+%% GUI with interactive response-plot updates
+% https://www.mathworks.com/help/control/ug/build-app-with-interactive-plot-updates.html
+% display the step response of a second-order dynamic system
+% of fixed natural frequency
+
+clc
+close all
+clear variables
+
+% initial values
+zeta = 0.5; % damping ratio
+wn = 2; % natural frequency
+sys = tf(wn^2, [1, 2*zeta*wn, wn^2]);
+
+f = figure;
+ax = axes('Parent',f, 'position',[0.13, 0.39, 0.77, 0.54]);
+h = stepplot(ax, sys);
+setoptions(h, 'Xlim',[0, 10], 'Ylim',[0, 2]);
+
+b = uicontrol('Parent',f, 'Style','slider', 'Position',[81, 54, 419, 23], ...
+    'value',zeta, 'min',0, 'max',1);
+bgcolor = f.Color;
+bl1 = uicontrol('Parent',f, 'Style','text', 'Position',[50, 54, 23, 23], ...
+    'String','0', 'BackgroundColor',bgcolor);
+bl2 = uicontrol('Parent',f, 'Style','text', 'Position',[500, 54, 23, 23], ...
+    'String','1', 'BackgroundColor',bgcolor);
+bl3 = uicontrol('Parent',f, 'Style','text', 'Position',[240, 25, 100, 23], ...
+    'String','Damping Ratio', 'BackgroundColor',bgcolor);
+
+b.Callback = @(es, ed) updateSystem(h, tf(wn^2, [1, 2*(es.Value)*wn, wn^2]));
+
 %% check validity of Ktrace2.m
 clc
 close all
