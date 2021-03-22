677001f30f4a46a26c35569cdb55405704148fef
Haedong Kim <haedong.kim90@gmail.com>
2021-03-22 15:39:36 -0400

    Sensitivity analysis for IKtof

--- sensitivity_analysis/sa_iktof.R
+++ sensitivity_analysis/sa_iktof.R
@@ -5,6 +5,13 @@ source("./sensitivity_analysis/kcurrents.R")
 source("./sensitivity_analysis/outputs.R")
 eps <- .Machine$double.eps
 
+# sensitivity analysis arguments
+nvar <- 13
+N <- 10000
+G <- 30
+grid <- seq(0, 1, length=G)
+XX <- matrix(NA, ncol=nvar, nrow=N)
+
 scale_param <- function(unit_param) {
   # parameters ranges
   p1r <- c(-50, 60)
@@ -39,6 +46,45 @@ scale_param <- function(unit_param) {
   return(param)
 }
 
+main_effect <- function(gpi) {
+  m <- q1 <- q2 <- matrix(NA, ncol=nvar, nrow=G)
+  for(j in 1:nvar) {
+    for(i in 1:G) {
+      XX[,j] <- grid[i]
+      XX[,-j] <- randomLHS(N, nvar-1)
+      
+      p <- predGPsep(gpi, XX, lite=TRUE, nonug=TRUE)
+      
+      m[i,j] <- mean(p$mean)
+      q1[i,j] <- mean(qnorm(0.05, p$mean, sqrt(p$s2)))
+      q2[i,j] <- mean(qnorm(0.95, p$mean, sqrt(p$s2)))
+    }
+  }
+
+  return(list(m=m, q1=q1, q2=q2))
+}
+
+first_sensitivity <- function(gpi) {
+  M <- randomLHS(N, nvar)
+  pM <- predGPsep(gpi, M, lite=TRUE, nonug=TRUE)
+  Ey <- mean(pM$mean)
+  Vary <- (t(pM$mean) %*% pM$mean)/N - Ey^2
+
+  Mprime <- randomLHS(N, nvar)
+  S <- EE2j <- rep(NA, nvar)
+  for(j in 1:nvar) {
+    Mjprime <- Mprime
+    Mjprime[,j] <- M[,j]
+    
+    pMprime <- predGPsep(gpi, Mjprime, lite=TRUE, nonug=TRUE)
+    EE2j[j] <- (t(pM$mean) %*% pMprime$mean)/(N - 1)
+    S[j] <- (EE2j[j] - Ey^2)/Vary
+  }
+
+  return(S)
+}
+
+## data preparation -----
 # input arguments
 hold_volt <- -70
 volt <- 50
@@ -56,7 +102,6 @@ time_space[[2]] <- th
 time_space[[3]] <- tp_adj
 
 # initial data with LHS
-nvar <- 13
 X <- randomLHS(1300, nvar)
 Y <- matrix(NA, ncol=7, nrow=nrow(X)) # 7: number of outputs]
 na_row_idx <- NULL
@@ -79,7 +124,63 @@ for(i in 1:nrow(X)) {
 X <- X[-na_row_idx, ]
 Y <- Y[-na_row_idx, ]
 
-# fit
-out <- Y[,2]
-gpi <- newGPsep(X, out, d=0.1, g=var(out)/10, dK=TRUE)
-mle <- mleGPsep(gpi, param="both", tmin=c(eps,2), tmax=c(10, var(out)))
+## sensitivity analysis -----
+# output 1
+out1 <- Y[,1]
+gpi <- newGPsep(X, out1, d=0.1, g=var(out1)/10, dK=TRUE)
+mle <- mleGPsep(gpi, param="both", tmin=c(eps,2), tmax=c(10,var(out1)))
+
+# main effects
+m1 <- q11 <- q21 <- matrix(NA, ncol=nvar, nrow=G)
+for(j in 1:nvar) {
+  for(i in 1:G) {
+    XX[,j] <- grid[i]
+    XX[,-j] <- randomLHS(N, nvar-1)
+    
+    p <- predGPsep(gpi, XX, lite=TRUE, nonug=TRUE)
+    
+    m1[i,j] <- mean(p$mean)
+    q11[i,j] <- mean(qnorm(0.05, p$mean, sqrt(p$s2)))
+    q12[i,j] <- mean(qnorm(0.95, p$mean, sqrt(p$s2)))
+  }
+}
+
+# first-order sensitivity
+M <- randomLHS(N, nvar)
+pM <- predGPsep(gpi, M, lite=TRUE, nonug=TRUE)
+Ey <- mean(pM$mean)
+Vary <- (t(pM$mean) %*% pM$mean)/N - Ey^2
+
+Mprime <- randomLHS(N, nvar)
+S <- EE2j <- rep(NA, nvar)
+for(j in 1:nvar) {
+  Mjprime <- Mprime
+  Mjprime[,j] <- M[,j]
+  
+  pMprime <- predGPsep(gpi, Mjprime, lite=TRUE, nonug=TRUE)
+  EE2j[j] <- (t(pM$mean) %*% pMprime$mean)/(N - 1)
+  S[j] <- (EE2j[j] - Ey^2)/Vary
+}
+
+deleteGPsep(gpi)
+
+# output 2 
+out2 <- Y[,2]
+gpi <- newGPsep(X, out2, d=0.1, g=var(out2)/100, dK=TRUE)
+mle <- mleGPsep(gpi, param="both", tmin=c(eps,2), tmax=c(10,var(out2)/10))
+
+me2 <- main_effect(gpi)
+S2 <- first_sensitivity(gpi)
+
+deleteGPsep(gpi)
+
+## visualize
+m <- me2$m
+q1 <- me2$q1
+q2 <- me2$q2
+plot(0, xlab="grid", ylab="main effect", xlim=c(0,1), ylim=range(c(q1,q2)), type="n")
+for(j in 1:nvar) {
+  lines(grid, m[,j], col=j, lwd=2)
+}
+legend("bottomright", paste0("x",1:nvar), fill=1:nvar, horiz=TRUE, cex=0.75)
+
