3d69f7e50cad12c79653a1992168d3d46f906d0c
Haedong Kim <haedong.kim90@gmail.com>
2021-03-23 23:18:26 -0400

    Re-parameterized IKtos

--- Kv_app/IKtos.m
+++ Kv_app/IKtos.m
@@ -4,8 +4,8 @@ function [current_trc] = IKtos(param, holdV, P1, time_space)
 
     act0 = 0.417069e-3;  % ato_s; Gating variable for transient outward K+ current
     inact0 = 0.998543;  % ito_s; Gating variable for transient outward K+ current
-    gmax = param(12); % 0.0629 in spetal model; 0 in apical model
-    Ek = param(13); % resting potential
+    gmax = param(10); % 0.0629 in spetal model; 0 in apical model
+    Ek = param(11); % resting potential
 
     % time space information
     t = time_space{1};
@@ -17,27 +17,29 @@ function [current_trc] = IKtos(param, holdV, P1, time_space)
 
     % at holding potential
     cv_hold = cv(param(1:11), holdV);
-    act_hold = cv_hold(1) - (cv_hold(1) - act0).*exp(-(tH./cv_hold(2)));
-    inact_hold = cv_hold(3) - (cv_hold(3) - inact0).*exp(-(tH./cv_hold(4)));
+    act_hold = cv_hold(1) - (cv_hold(1) - act0).*exp(-(tH./cv_hold(3)));
+    inact_hold = cv_hold(2) - (cv_hold(2) - inact0).*exp(-(tH./cv_hold(4)));
     current_trc(1:hold_idx) = gmax.*(act_hold).*(inact_hold).*(holdV - Ek);
 
     % at P1
     cv_P1 = cv(param(1:11), P1);
-    act_P1 = cv_P1(1) - (cv_P1(1) - act0).*exp(-(tP1_adj./cv_P1(2)));
-    inact_P1 = cv_P1(3) - (cv_P1(3) - inact0).*exp(-(tP1_adj./cv_P1(4)));
+    act_P1 = cv_P1(1) - (cv_P1(1) - act0).*exp(-(tP1_adj./cv_P1(3)));
+    inact_P1 = cv_P1(2) - (cv_P1(2) - inact0).*exp(-(tP1_adj./cv_P1(4)));
     current_trc((hold_idx + 1):end) = gmax.*(act_P1).*(inact_P1).*(P1 - Ek);
 end
 
 function [cv] = cv(p, V)
-    % p0 = [22.5, 7.7, 0.493, 0.0629, 2.058, 45.2, 5.7, 270, 1050, 45.2, 5.7]
-    cv = zeros(4, 1);
-    
     % note similarity of IKtos and IKur
     % same activation gate characteristics
-    cv(1) = 1 ./ (1 + exp(-(V+p(1))./p(2)));
-    cv(2) = p(3).*exp(-p(4).*V) + p(5);
 
-    % for inactivation gate, cv(3) is same but not for cv(4) 
-    cv(3) = 1 ./ (1 + exp((V+p(6))./p(7)));
-    cv(4) = p(8) + p(9)./(1 + exp((V + p(10))./p(11)));
+    % p0 = [22.5, 7.7, 
+    cv = zeros(4, 1);
+    
+    % steady-state values
+    cv(1) = 1 ./ (1 + exp(-(V+p(1))./p(2)));
+    cv(2) = 1 ./ (1 + exp((V+p(3))./p(4)));
+    
+    % time constants
+    cv(3) = p(5).*exp(-p(6).*V) + p(7);
+    cv(4) = p(8) + p(9)./(1 + exp((V + p(3))./p(4)));
 end
