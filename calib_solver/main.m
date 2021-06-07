aa0c688f30a4e8a2292ba663f62b957856599c8f
Haedong Kim <haedong.kim90@gmail.com>
2021-06-07 10:52:03 -0400

    fgoalattain

--- calib_solver/main.m
+++ calib_solver/main.m
@@ -8,7 +8,7 @@ clear variables
 trace_data = table2array(readtable('./data/wt-preprocessed/15o29009.xlsx'));
 t = trace_data(:,1);
 
-volt_steps = 1:11;
+volt_steps = 5:11;
 min_volt = -50;
 volts = zeros(length(volt_steps), 1);
 for i = 1:length(volt_steps)
@@ -65,6 +65,8 @@ hold_volt = -70;
 Ek = -91.1;
 
 % optimization constraints 
+goal = [0, 0];
+weight = [1, 1];
 A = [];
 b = [];
 Aeq = [];
@@ -82,8 +84,11 @@ low = p0 - 2*p0;
 high = p0 + 2*p0;
 low([5, 6, 7, 8, 9, 10, 14, 15, 16, 17, 18, 19, 20]) = eps;
 
-options = optimoptions('fmincon', 'MaxFunctionEvaluations',1e+5);
-opt_fun = @(p) obj_multi(p, hold_volt, hold_idx, volts, t, yksum, Ek, true, false);
+options = optimoptions('fgoalattain', 'MaxFunctionEvaluations',1e+6);
+% obj_multi2(p0, hold_volt, hold_idx, volts, t, yksum, Ek, true);
+opt_fun = @(p) [obj_ks(p, hold_volt, hold_idx, volts, t, yksum, Ek, true);
+    obj_rmse(p, hold_volt, hold_idx, volts, t, yksum, Ek, true, false)];
+% opt_fun = @(p) obj_multi(p, hold_volt, hold_idx, volts, t, yksum, Ek, true, false);
 % opt_fun = @(p) obj_rmse(p, hold_volt, hold_idx, volts, t, yksum, Ek, true, false);
 
 num_iters = 1;
@@ -91,7 +96,7 @@ rmse_list = zeros(num_iters, 1);
 sol_list = cell(num_iters, 1);
 
 % first run with p0
-[sol, fval] = fmincon(opt_fun, p0, A, b, Aeq, beq, lb, ub, nonlcon, options);
+[sol, fval] = fgoalattain(opt_fun, p0, goal, weight, A, b, Aeq, beq, lb, ub, nonlcon, options);
 sol_list{1} = sol;
 rmse_list(1) = fval;
 
@@ -185,7 +190,7 @@ clear variables
 trace_data = table2array(readtable('./data/ko-preprocessed/15n24005.xlsx'));
 t = trace_data(:,1);
 
-volt_steps = 1:11;
+volt_steps = 5:11;
 min_volt = -50;
 volts = zeros(length(volt_steps), 1);
 for i = 1:length(volt_steps)
@@ -260,9 +265,10 @@ high = p0 + 2*p0;
 low([5, 6, 7, 8, 9, 10, 14, 15, 16, 17, 18, 19, 20]) = eps;
 
 options = optimoptions('fmincon', 'MaxFunctionEvaluations',1e+5);
-opt_fun = @(p) obj_rmse(p, hold_volt, hold_idx, volts, t, yksum, Ek, true, false);
+opt_fun = @(p) obj_multi(p, hold_volt, hold_idx, volts, t, yksum, Ek, true, false);
+% opt_fun = @(p) obj_rmse(p, hold_volt, hold_idx, volts, t, yksum, Ek, true, false);
 
-num_iters = 30;
+num_iters = 1;
 rmse_list = zeros(num_iters, 1);
 sol_list = cell(num_iters, 1);
 
---
+++ calib_solver/obj_ks.m
@@ -0,0 +1,58 @@
+function [z] = obj_multi2(p, hold_volt, hold_idx, volts, t, yksum, Ek, param_select)
+    num_volts = length(volts);
+    pts_diff = zeros(num_volts, 1);
+
+    for i = 1:num_volts
+        volt = volts(i);
+        yksum_i = yksum(:, i);
+        
+        time_space = cell(1, 3);
+        time_space{1} = t;
+        time_space{2} = t(1:hold_idx(i));
+        pulse_t = t(hold_idx(i)+1:end);
+        pulse_t_adj = pulse_t - pulse_t(1);
+        time_space{3} = pulse_t_adj;
+        
+        % generate current
+        if param_select == true
+            [~, ~, ~, ~, yksum_hat] = reduced_model(p, hold_volt, volt, time_space, Ek);
+        elseif param_select == false        
+            [~, ~, ~, ~, yksum_hat] = full_model(p, hold_volt, volt, time_space, Ek);
+        end
+
+        % check validty of trace shape
+        [~, peak_idx] = max(yksum_hat);
+        check_pt1 = any(isnan(yksum_hat));
+        check_pt2 = any(yksum_hat < 0);
+        check_pt3 = var(yksum_hat(1:hold_idx(i))) > 0.01; % not stable at hold_volt
+        check_pt4 = peak_idx < hold_idx(i); % not stable at hold_volt of too flat at pulse
+        
+        if(check_pt1 || check_pt2 || check_pt3 || check_pt4)
+            pts_diff(i) = 1e+3; % arbitrary big number
+        else
+            % calculate differences of key points
+            yksum_key_pts = key_points(time_space, yksum_i);
+            yksum_hat_key_pts = key_points(time_space, yksum_hat);
+            pts_diff(i) = sqrt(mean((yksum_key_pts - yksum_hat_key_pts).^2));
+        end
+    end    
+    z = sum(pts_diff);
+end
+
+function [pts] = key_points(time_space, current_trace)
+    % peak and end point
+    pts = zeros(2, 1);
+    
+    hold_idx = length(time_space{2});
+    pulse_t_adj = time_space{3};
+    
+    % early 20%
+    early_phase_idx = floor(length(pulse_t_adj)*0.2);
+    
+    % 1st time stamp from yksum
+    current_trace_trunc = current_trace((hold_idx+1):end);
+    current_trace_trunc = current_trace_trunc(1:early_phase_idx);
+
+    pts(1) = max(current_trace_trunc);
+    pts(2) = current_trace(end);
+end
