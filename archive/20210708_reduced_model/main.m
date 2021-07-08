29077ae6bbf0ed79d4d470930c29f87c25f03fd8
Haedong Kim <haedong.kim90@gmail.com>
2021-07-08 17:55:48 -0400

    Archived

---
+++ archive/20210708_reduced_model/main.m
@@ -0,0 +1,355 @@
+%% WT
+% 4.5-sec voltage-dependent data
+clc
+close all
+clear variables
+
+% load experimental data
+trace_data = table2array(readtable('./data/wt-preprocessed/15o29009.xlsx'));
+t = trace_data(:,1);
+
+volt_steps = 6:11;
+min_volt = -50;
+volts = zeros(length(volt_steps), 1);
+for i = 1:length(volt_steps)
+    volts(i) = min_volt + (volt_steps(i)-1)*10;
+end
+
+yksum = trace_data(:, (volt_steps + 1));
+[~, num_volts] = size(yksum);
+
+% visualize experimental data
+figure(1)
+plot(t, yksum(:, 1))
+hold on
+for i=2:num_volts
+    plot(t, yksum(:, i))
+end
+hold off
+axis tight
+xlabel('Time (ms)')
+ylabel('Current (pA/pF)')
+
+% estimate holding time
+ideal_hold_time = 0.120*1000;
+[~, ideal_hold_idx] = min(abs(t-ideal_hold_time));
+init_stable_val = sqrt(var(yksum(ideal_hold_idx,:)));
+
+hold_idx = zeros(num_volts,1);
+for i = 1:num_volts
+    trc = yksum(:,i);
+
+    counter = 1;
+    statbility_est = abs(trc(ideal_hold_idx+counter) - trc(ideal_hold_idx-counter));
+    if statbility_est > 10*init_stable_val
+        hold_idx(i) = ideal_hold_idx;
+        continue
+    end
+    
+    while true
+        % update counter and stable value
+        counter = counter + 1;
+        stable_val = statbility_est;
+        
+        % check stability
+        statbility_est = abs(trc(ideal_hold_idx+counter) - trc(ideal_hold_idx-counter));
+        if statbility_est > 10*stable_val
+            hold_idx(i) = ideal_hold_idx + (counter-1);
+            break
+        end
+    end
+end
+
+% protocol
+hold_volt = -70;
+Ek = -91.1;
+
+% optimization constraints 
+A = [];
+b = [];
+Aeq = [];
+beq = [];
+lb = [];
+ub = [];
+nonlcon = [];
+
+p0 = [33,15.5,20,8,7,0.3956,0.00095,0.051335,0.14067,0.387,22.5,45.2,40,2.058,803,0.05766,0.07496,5334,13.17,0.0428];
+lb = p0 - 10*p0;
+ub = p0 + 10*p0;
+lb([5, 6, 7, 8, 9, 10, 14, 15, 16, 17, 18, 19, 20]) = eps;
+
+low = p0 - 2*p0;
+high = p0 + 2*p0;
+low([5, 6, 7, 8, 9, 10, 14, 15, 16, 17, 18, 19, 20]) = eps;
+
+options = optimoptions('fmincon', 'MaxFunctionEvaluations',1e+6);
+opt_fun = @(p) obj_multi2(p, hold_volt, hold_idx, volts, t, yksum, Ek, true, true);
+% opt_fun = @(p) obj_rmse(p, hold_volt, hold_idx, volts, t, yksum, Ek, true, false);
+
+num_iters = 30;
+rmse_list = zeros(num_iters, 1);
+sol_list = cell(num_iters, 1);
+
+% first run with p0
+[sol, fval] = fmincon(opt_fun, p0, A, b, Aeq, beq, lb, ub, nonlcon, options);
+sol_list{1} = sol;
+rmse_list(1) = fval;
+
+for i = 2:num_iters
+    fprintf('[%i/%i] \n', i, num_iters)
+    
+    % random intialization
+    running_p0 = zeros(length(p0), 1);
+    for j = 1:length(p0)
+        unif_dist = makedist('Uniform', 'lower',low(j), 'upper',high(j));
+        running_p0(j) = random(unif_dist, 1);
+    end
+    
+    % optimization
+    [sol, fval] = fmincon(opt_fun, running_p0, A, b, Aeq, beq, lb, ub, nonlcon, options);
+    sol_list{i} = sol;
+    rmse_list(i) = fval;
+end
+
+[~, best_fit_idx] = min(rmse_list);
+sol = sol_list{best_fit_idx};
+
+% visualization calibration result
+volt_idx = 30;
+volt = volts(volt_idx);
+
+time_space = cell(1,3);
+time_space{1} = t;
+time_space{2} = t(1:hold_idx(volt_idx));
+pulse_t = t(hold_idx(volt_idx)+1:end);
+pulse_t_adj = pulse_t - pulse_t(1);
+time_space{3} = pulse_t_adj;
+
+[~, ~, ~, ~, yksum_hat] = reduced_model(sol, hold_volt, volt, time_space, Ek);
+figure(2)
+plot(t, yksum_hat)
+hold on
+for i=2:num_volts
+    volt_idx = i;
+    volt = volts(volt_idx);
+    
+    time_space = cell(1,3);
+    time_space{1} = t;
+    time_space{2} = t(1:hold_idx(volt_idx));
+    pulse_t = t(hold_idx(volt_idx)+1:end);
+    pulse_t_adj = pulse_t - pulse_t(1);
+    time_space{3} = pulse_t_adj;
+
+    [~, ~, ~, ~, yksum_hat] = reduced_model(sol, hold_volt, volt, time_space, Ek);
+    plot(t, yksum_hat)
+end
+hold off
+axis tight
+xlabel('Time (ms)')
+ylabel('Current (pA/pF)')
+
+%% plot for each voltage
+clc
+close all
+
+peak = zeros(num_volts, 1);
+for i = 1:num_volts
+    volt = volts(i);
+    
+    time_space = cell(1,3);
+    time_space{1} = t;
+    time_space{2} = t(1:hold_idx(i));
+    pulse_t = t(hold_idx(i)+1:end);
+    pulse_t_adj = pulse_t - pulse_t(1);
+    time_space{3} = pulse_t_adj;
+    
+    [~, ~, ~, ~, yksum_hat] = reduced_model(sol, hold_volt, volt, time_space, Ek);
+    peak(i) = max(yksum_hat);
+    
+    figure(i)
+    plot(t, yksum(:, i))
+    hold on
+    plot(t, yksum_hat)
+    hold off
+    axis tight
+    xlabel('Time (ms)')
+    ylabel('Current (pA/pF)')
+    legend('Experimental','Model Prediction')
+end
+
+%% same routine for Mgat1KO
+% 4.5-sec voltage-dependent data
+clc
+close all
+clear variables
+
+% load experimental data
+trace_data = table2array(readtable('./data/ko-preprocessed/15n24005.xlsx'));
+t = trace_data(:,1);
+
+volt_steps = 5:11;
+min_volt = -50;
+volts = zeros(length(volt_steps), 1);
+for i = 1:length(volt_steps)
+    volts(i) = min_volt + (volt_steps(i)-1)*10;
+end
+
+yksum = trace_data(:, (volt_steps + 1));
+[~, num_volts] = size(yksum);
+
+% visualize experimental data
+figure(1)
+plot(t, yksum(:, 1))
+hold on
+for i=2:num_volts
+    plot(t, yksum(:, i))
+end
+hold off
+axis tight
+xlabel('Time (ms)')
+ylabel('Current (pA/pF)')
+
+% estimate holding time
+ideal_hold_time = 0.120*1000;
+[~, ideal_hold_idx] = min(abs(t-ideal_hold_time));
+init_stable_val = sqrt(var(yksum(ideal_hold_idx,:)));
+
+hold_idx = zeros(num_volts,1);
+for i = 1:num_volts
+    trc = yksum(:,i);
+
+    counter = 1;
+    statbility_est = abs(trc(ideal_hold_idx+counter) - trc(ideal_hold_idx-counter));
+    if statbility_est > 10*init_stable_val
+        hold_idx(i) = ideal_hold_idx;
+        continue
+    end
+    
+    while true
+        % update counter and stable value
+        counter = counter + 1;
+        stable_val = statbility_est;
+        
+        % check stability
+        statbility_est = abs(trc(ideal_hold_idx+counter) - trc(ideal_hold_idx-counter));
+        if statbility_est > 10*stable_val
+            hold_idx(i) = ideal_hold_idx + (counter-1);
+            break
+        end
+    end
+end
+
+% protocol
+hold_volt = -70;
+Ek = -91.1;
+
+% optimization constraints 
+A = [];
+b = [];
+Aeq = [];
+beq = [];
+lb = [];
+ub = [];
+nonlcon = [];
+
+p0 = [33,15.5,20,8,7,0.3956,0.00095,0.051335,0.14067,0.387,22.5,45.2,40,2.058,803,0.05766,0.07496,5334,13.17,0.0428];
+lb = p0 - 10*p0;
+ub = p0 + 10*p0;
+lb([5, 6, 7, 8, 9, 10, 14, 15, 16, 17, 18, 19, 20]) = eps;
+
+low = p0 - 2*p0;
+high = p0 + 2*p0;
+low([5, 6, 7, 8, 9, 10, 14, 15, 16, 17, 18, 19, 20]) = eps;
+
+options = optimoptions('fmincon', 'MaxFunctionEvaluations',1e+5);
+opt_fun = @(p) obj_multi(p, hold_volt, hold_idx, volts, t, yksum, Ek, true, false);
+% opt_fun = @(p) obj_rmse(p, hold_volt, hold_idx, volts, t, yksum, Ek, true, false);
+
+num_iters = 1;
+rmse_list = zeros(num_iters, 1);
+sol_list = cell(num_iters, 1);
+
+% first run with p0
+[sol, fval] = fmincon(opt_fun, p0, A, b, Aeq, beq, lb, ub, nonlcon, options);
+sol_list{1} = sol;
+rmse_list(1) = fval;
+
+for i = 2:num_iters
+    fprintf('[%i/%i] \n', i, num_iters)
+    
+    % random intialization
+    running_p0 = zeros(length(p0), 1);
+    for j = 1:length(p0)
+        unif_dist = makedist('Uniform', 'lower',low(j), 'upper',high(j));
+        running_p0(j) = random(unif_dist, 1);
+    end
+    
+    % optimization
+    [sol, fval] = fmincon(opt_fun, running_p0, A, b, Aeq, beq, lb, ub, nonlcon, options);
+    sol_list{i} = sol;
+    rmse_list(i) = fval;
+end
+
+[~, best_fit_idx] = min(rmse_list);
+sol = sol_list{best_fit_idx};
+
+% visualization calibration result
+volt_idx = 1;
+volt = volts(volt_idx);
+
+time_space = cell(1,3);
+time_space{1} = t;
+time_space{2} = t(1:hold_idx(volt_idx));
+pulse_t = t(hold_idx(volt_idx)+1:end);
+pulse_t_adj = pulse_t - pulse_t(1);
+time_space{3} = pulse_t_adj;
+
+[~, ~, ~, ~, yksum_hat] = reduced_model(sol, hold_volt, volt, time_space, Ek);
+figure(2)
+plot(t, yksum_hat)
+hold on
+for i=2:num_volts
+    volt_idx = i;
+    volt = volts(volt_idx);
+    
+    time_space = cell(1,3);
+    time_space{1} = t;
+    time_space{2} = t(1:hold_idx(volt_idx));
+    pulse_t = t(hold_idx(volt_idx)+1:end);
+    pulse_t_adj = pulse_t - pulse_t(1);
+    time_space{3} = pulse_t_adj;
+
+    [~, ~, ~, ~, yksum_hat] = reduced_model(sol, hold_volt, volt, time_space, Ek);
+    plot(t, yksum_hat)
+end
+hold off
+axis tight
+xlabel('Time (ms)')
+ylabel('Current (pA/pF)')
+
+%% plot for each voltage
+clc
+close all
+
+for i = 1:num_volts
+    volt = volts(i);
+    
+    time_space = cell(1,3);
+    time_space{1} = t;
+    time_space{2} = t(1:hold_idx(i));
+    pulse_t = t(hold_idx(i)+1:end);
+    pulse_t_adj = pulse_t - pulse_t(1);
+    time_space{3} = pulse_t_adj;
+    
+    [~, ~, ~, ~, yksum_hat] = reduced_model(sol, hold_volt, volt, time_space, Ek);
+    
+    figure(i)
+    plot(t, yksum(:, i))
+    hold on
+    plot(t, yksum_hat)
+    hold off
+    axis tight
+    xlabel('Time (ms)')
+    ylabel('Current (pA/pF)')
+    legend('Experimental','Model Prediction')
+end
---
+++ archive/20210708_reduced_model/reduced_model.m
@@ -0,0 +1,53 @@
+function [ykto, ykslow1, ykslow2, ykss, yksum] = reduced_model(p, hold_volt, volt, time_space, Ek)
+    % not synchronize the shared parameters of IKslo1, IKslo2, and IKss
+    % default values
+    kto0 = [33, 15.5, 20, 16, 8, 7, 0.03577, 0.06237, 0.18064, 0.3956, ...
+        0.000152, 0.067083, 0.00095, 0.051335, 0.2087704319, 0.14067, 0.387];
+    kslow10 = [22.5, 45.2, 40.0, 7.7, 5.7, 6.1, 0.0629, 2.058, 803.0, 18.0, 0.9214774521, 0.05766, 0.07496];
+    kslow20 = [22.5, 45.2, 40.0, 7.7, 5.7, 6.1, 0.0629, 2.058, 5334, 4912, 0.05766];
+    kss0 = [22.5, 40.0, 7.7, 0.0862, 1235.5, 13.17, 0.0428];
+
+    param_kto = zeros(17,1);
+    param_kslow1 = zeros(13,1);
+    param_kslow2 = zeros(11,1);
+    param_kss = zeros(7,1);
+
+    % fixed parameters for Ikto: p4, p7, p8, p9, p11, p12, fkto
+    fixed_kto_idx = [4, 7, 8, 9, 11, 12, 15];
+    param_kto(fixed_kto_idx) = kto0(fixed_kto_idx);
+    
+    % tuning parameters for Ikto
+    tune_kto_idx = setdiff(1:17, fixed_kto_idx);
+    param_kto(tune_kto_idx) = p(1:10);
+
+    % fixed parameters for Ikslow1: p4, p5, p6, p7, p10, fkslow1
+    fixed_kslow1_idx = [4, 5, 6, 7, 10, 11];
+    param_kslow1(fixed_kslow1_idx) = kslow10(fixed_kslow1_idx);
+
+    % tuning parameters for Ikslow1
+    tune_kslow1_idx = setdiff(1:13, fixed_kslow1_idx);
+    param_kslow1(tune_kslow1_idx) = p(11:17);
+
+    % fixed parameters for Ikslow2
+    fixed_kslow2_idx = [4, 5, 6, 7, 10];
+    param_kslow2(fixed_kslow2_idx) = kslow20(fixed_kslow2_idx);
+
+    % tuning parameters for Ikslow2
+    tune_kslow2_idx = setdiff(1:11, fixed_kslow2_idx);
+    param_kslow2(tune_kslow2_idx) = p([11, 12, 13, 14, 18, 16]);
+    
+    % fixed parameters for Ikss
+    fixed_kss_idx = [3, 4, 5];
+    param_kss(fixed_kss_idx) = kss0(fixed_kss_idx);
+
+    % tuning parameters for Ikss
+    tune_kss_idx = setdiff(1:7, fixed_kss_idx);
+    param_kss(tune_kss_idx) = p([11, 13, 19, 20]);
+
+    % generate K+ currents
+    ykto = ikto(param_kto, hold_volt, volt, time_space, Ek);
+    ykslow1 = ikslow1(param_kslow1, hold_volt, volt, time_space, Ek);
+    ykslow2 = ikslow2(param_kslow2, hold_volt, volt, time_space, Ek);
+    ykss = ikss(param_kss, hold_volt, volt, time_space, Ek);
+    yksum = ykto + ykslow1 + ykslow2 + ykss;
+end
