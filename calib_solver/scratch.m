8911f10ed19e12291a39936ad8e47dac008bc245
Haedong Kim <haedong.kim90@gmail.com>
2021-08-22 18:33:01 -0400

    Check calibration result

--- calib_solver/scratch.m
+++ calib_solver/scratch.m
@@ -1,3 +1,91 @@
+%% check calibration result for general kcurrent model
+clc
+close all 
+clear variables
+format shortG
+
+% specify model structure
+current_names = {'ikto', 'ikslow1', 'ikss', 'ik1'};
+num_currents = length(current_names);
+
+tune_idx1_kto = [1, 2, 15, 16, 17];
+tune_idx1_kslow1 = [1, 2, 3, 9, 12, 13];
+tune_idx1_kss = [4, 5, 6, 7];
+tune_idx1_k1 = [1, 3, 5, 7];
+idx_info1 = {tune_idx1_kto, ...
+    tune_idx1_kslow1, ...
+    tune_idx1_kss, ...
+    tune_idx1_k1};
+
+idx_info2 = cell(1, num_currents);
+cul_idx_len = 0;
+for i = 1: num_currents
+    cul_idx_len_old = cul_idx_len;
+    cul_idx_len = cul_idx_len + length(idx_info1{i});
+    idx_info2{i} = (1+cul_idx_len_old):(cul_idx_len);
+end
+
+model_info = [current_names; idx_info1; idx_info2];
+field_names = {'name', 'idx1', 'idx2'};
+model_struct = cell2struct(model_info, field_names, 1);
+
+% specify result file to check
+file_group = 'ko';
+file_name = '15n03002.xlsx';
+calib = table2array(readtable(fullfile(pwd, file_group, strcat('calib_param_', file_name))));
+
+sol = zeros(cul_idx_len, 1);
+for i=1:num_currents
+    running_calib = calib(:, i);
+    running_calib(~isnan(running_calib));
+    
+    sol(idx_info2{i}) = running_calib(idx_info1{i});
+end
+
+% experimental data to be compared
+exp_data = table2array(readtable(fullfile(pwd,'data', strcat(file_group, '-preprocessed2'), file_name)));
+t = exp_data(:, 1);
+yksum = exp_data(:, 2:end);
+
+% protocol
+protocol = cell(4, 1);
+
+hold_volt = -70;
+volts = -50:10:50;
+ideal_hold_time = 120;
+ideal_end_time = 4.6*1000;
+ek = -91.1;
+
+[~, ideal_hold_idx] = min(abs(t - ideal_hold_time));
+[~, ideal_end_idx] = min(abs(t - ideal_end_time));
+
+t = t(1:ideal_end_idx);
+yksum = yksum(1:ideal_end_idx, :);
+
+time_space = cell(1, 3);
+time_space{1} = t;
+time_space{2} = t(1:ideal_hold_idx);
+pulse_t = t(ideal_hold_idx+1:end);
+pulse_t_adj = pulse_t - pulse_t(1);
+time_space{3} = pulse_t_adj;
+
+protocol{1} = hold_volt;
+protocol{3} = time_space;
+protocol{4} = ek;
+
+% generate yksum_hat
+for i=1:length(volts)
+    protocol{2} = volts(i);
+    [yksum_hat, ~] = kcurrent_model(sol, model_struct, protocol);
+
+    figure(i)
+    plot(t, yksum(:, i))
+    hold on
+    plot(t, yksum_hat)
+    hold off
+    legend('Experimental','Model')
+end
+
 %% test general kcurrent model
 clc
 close all
