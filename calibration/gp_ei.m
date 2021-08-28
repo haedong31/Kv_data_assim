281579aee59b58a336bc763e9b3f0080d8d60b15
Haedong Kim <haedong.kim90@gmail.com>
2021-08-28 15:55:21 -0400

    Main body of calibration

--- calibration/gp_ei.m
+++ calibration/gp_ei.m
@@ -1,16 +1,16 @@
 clc
 close all
 clear variables
+format shortG
 
-%
+%%%%%
 % code arguments for calibration
-%
+%%%%%
 group_name = 'wt';
 save_dir = strcat('calib_result1_', group_name);
 
 % selection of currents
 current_names = {'ikto', 'ikslow1', 'ikss'};
-num_currents = length(current_names);
 
 % tunning index in individual current models
 tune_idx1_kto = [1, 2, 3, 5, 6, 15, 16, 17];
@@ -20,24 +20,97 @@ tune_idx1_kss = [3, 4];
 tune_idx1_kur = [1, 3];
 tune_idx1_k1 = [1, 3, 5, 7];
 
+% protocol
+hold_volt = -70;
+volts = -50:10:50;
+ideal_hold_time = 120;
+ideal_end_time = 4.6*1000;
+ek = -91.1;
 
+% optimization options
 reps = 100;
 ninit = 12;
 out_size = 50;
-prog_ei = NaN(reps, out_size);
-for r = 1:reps
-    [sol, fval, maxei] = optim_ei(@gpfun, ninit, 2, out_size);
-    running_prog_ei = bov(fval);
-    prog_ei(r, :) = running_prog_ei';
+
+%%%%%
+% main
+%%%%%
+% matching table
+matching_table = readtable(fullfile(pwd, 'data', strcat('matching-table-', group_name, '.xlsx')));
+[num_files, ~] = size(matching_table);
+mkdir(fullfile(pwd, save_dir));
+file_names = matching_table.trace_file_name_4half;
+
+% exclude row not having 4.5-sec data
+loop_idx = [];
+for i = 1:num_files
+    if isempty(file_names{i})
+        continue
+    end
+    loop_idx = [loop_idx, i];
+end
+num_files = length(loop_idx);
+
+global model_struct
+model_struct = gen_model_struct(current_names, tune_idx1_kto, tune_idx1_kslow1, ...
+    tune_idx1_kslow2, tune_idx1_kss, tune_idx1_kur, tune_idx1_k1);
+
+global volt_space
+volt_space = cell(3, 1);
+volt_space{1} = hold_volt;
+volt_space{2} = volts;
+volt_space{3} = ek;
+
+global time_space
+global yksum
+
+for i = 1:floor(num_files/2)
+    fprintf('##### RUN')
+
+    % read data
+    file_path = fullfile(pwd, 'data', strcat(group_name, '-preprocessed2'), file_names{loop_idx(i)});
+    trace_data = table2array(readtable(file_path));
+
+    t = trace_data(:, 1);
+    yksum = trace_data(:, 2:end);
+
+    % estimate time points
+    [~, ideal_hold_idx] = min(abs(t - ideal_hold_time));
+    [~, ideal_end_idx] = min(abs(t - ideal_end_time));
+
+    t = t(1:ideal_end_idx);
+    yksum = yksum(1:ideal_end_idx, :);
+
+    % time space
+    time_space = cell(1, 3);
+    time_space{1} = t;
+    time_space{2} = t(1:ideal_hold_idx);
+    pulse_t = t(ideal_hold_idx+1:end);
+    pulse_t_adj = pulse_t - pulse_t(1);
+    time_space{3} = pulse_t_adj;
+    
+    % MC-style repetitions
+    prog = NaN(reps, out_size);
+    for r = 1:reps
+        [sol, fval, maxei] = optim_ei(@obj_rmse, ninit, 2, out_size);
+        running_prog = bov(fval);
+        prog(r, :) = running_prog';
+
+        fprintf('[File %i/%i] %s [Reps %i/%i] Min RMSE: %f \n', i, file_names{loop_idx(i)}, r, reps, min(running_prog));
+    end
 end
 
-plot(mean(prog_ei, 1))
+% plot bov
+plot(mean(prog, 1))
 hold on
 xline(ninit, '--', 'Color',[0.5,0.5,0.5])
 hold off
 
-% RMSE objective function; main optimization objective
+%%%%%
+% problem specific functions
+%%%%%
 function z = obj_rmse(p)
+    % RMSE objective function; main optimization objective
     global model_struct
     global volt_space
     global time_space
@@ -75,6 +148,48 @@ function z = obj_rmse(p)
     z = sum(rmse_list);
 end
 
+function scaledp = scale_param(p)
+end
+
+function model_struct = gen_model_struct(current_names)
+    num_currents = length(current_names);
+    
+    idx_info1 = cell(1, num_currents);
+    for i = 1:num_currents
+        switch current_names{i}
+        case "ikto"
+            idx_info1{i} = tune_idx1_kto;
+        case "ikslow1"
+            idx_info1{i} = tune_idx1_kslow1;
+        case "ikslow2"
+            idx_info1{i} = tune_idx1_kslow2;
+        case "ikss"
+            idx_info1{i} = tune_idx1_kss;
+        case "ikur"
+            idx_info1{i} = tune_idx1_kur;
+        case "ik1"
+            idx_info1{i} = tune_idx1_k1;
+        end
+    end
+
+    % tunning index in decision variable, p
+    idx_info2 = cell(1, num_currents);
+    cul_idx_len = 0;
+    for i = 1: num_currents
+        cul_idx_len_old = cul_idx_len;
+        cul_idx_len = cul_idx_len + length(idx_info1{i});
+        idx_info2{i} = (1+cul_idx_len_old):(cul_idx_len);
+    end
+
+    % create model structure
+    model_info = [current_names; idx_info1; idx_info2];
+    field_names = {'name', 'idx1', 'idx2'};
+    model_struct = cell2struct(model_info, field_names, 1);
+end
+
+%%%%%
+% surrogate-assisted optimization functions
+%%%%%
 function [sol, fval, maxei] = optim_ei(f, num_init_pt, num_var, out_size)
     % initialization
     initx = lhsdesign(num_init_pt, num_var);
@@ -108,8 +223,8 @@ function [sol, fval, maxei] = optim_ei(f, num_init_pt, num_var, out_size)
     end
 end
 
-% EI-based acquisition function
 function [xnew, fval] = ei_acquisition(x, y, gpmdl, num_multi_start, tol)
+    % EI-based acquisition function
     [fmin, min_idx] = min(y);
     [~, num_var] = size(x);
 
@@ -139,21 +254,21 @@ function [xnew, fval] = ei_acquisition(x, y, gpmdl, num_multi_start, tol)
     fval = fval(valid_idx);
 end
 
-% EI objective function in subroutine; - to formulate as minimization problem
 function z = obj_ei(x, fmin, gpmdl)
+    % EI objective function in subroutine; - to formulate as minimization problem
     z = -ei(x, fmin, gpmdl);
 end
 
-% expected improvement
 function z = ei(x, fmin, gpmdl)
+    % expected improvement
     [ypred, ysd, ~] = predict(gpmdl, x);
     d = fmin - ypred;
     dsig = d./ysd;
     z = d.*normcdf(dsig) + ysd.*normpdf(dsig);
 end
 
-% best objective value over course of procedure
 function prog = bov(y)
+    % best objective value over course of procedure
     ylen = length(y);
     prog = y;
 
