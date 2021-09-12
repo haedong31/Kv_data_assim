bedf2e55927a3f60e9f283f2df3a447b6ce3b757
Haedong Kim <haedong.kim90@gmail.com>
2021-09-12 04:17:32 -0400

    Reneamed

--- calibration/kcurrent_model4.m
+++ calibration/kcurrent_model4.m
@@ -1,4 +1,4 @@
-function [yksum, comp_currents] = kcurrent_model3(p, model_struct, protocol_info, pretrn_mdl)
+function [yksum, comp_currents] = kcurrent_model4(p, model_struct, protocol_info, pretrn_mdl)
     global param_kslow1
     
     num_currents = length(model_struct);
@@ -11,9 +11,10 @@ function [yksum, comp_currents] = kcurrent_model3(p, model_struct, protocol_info
     % declare shared parameters of ikslow1 as global variable
     matching_idx = strcmp(current_names, 'ikslow1');
     if any(matching_idx)    
-        num_kslow1_param = 13;
-        kslow1_default = [22.5, 45.2, 40.0, 7.7, 5.7, 6.1, 0.0629, ...
-            2.058, 803.0, 18.0, 0.9214774521, 0.05766, 0.07496];
+        num_kslow1_param = 11;
+
+        kslow1_default = [22.5, 45.2, 40.0, 7.7, 5.7, ...
+            6.1, 0.0629, 2.058, 803.0, 18.0, 0.05766];
         kslow1_default(pretrn_mdl(matching_idx).idx1) = pretrn_mdl(matching_idx).sol;
         
         tune_idx1 = model_struct(matching_idx).idx1;
@@ -52,9 +53,9 @@ function [current_trace] = gen_matching_current(p, model_info, idx, protocol_inf
     switch current_name
         case 'ikto'
             % generate ikto
-            num_param = 17;
-            kto_default = [33, 15.5, 20, 16, 8, 7, 0.03577, 0.06237, 0.18064, 0.3956, ...
-                0.000152, 0.067083, 0.00095, 0.051335, 0.2087704319, 0.14067, 0.387];
+            num_param = 16;
+            kto_default = [33.0, 40.0, 33.0, 15.5, 20.0, 7.7, 5.7, ... 
+                0.03577, 0.06237, 7.0, 0.18064, 0.3956, 0.0226585, 0.00095, 0.051335, 0.14067];
             kto_default(pretrn_mdl(idx).idx1) = pretrn_mdl(idx).sol;
 
             fixed_idx = setdiff(1:num_param, tune_idx1);
@@ -138,14 +139,10 @@ end
 
 function [current_trc] = ikto(p, hold_volt, volt, time_space, Ek)
     % constants & initial values
-    f_eacv = p(15); % 0.2087704319 Ikto fraction phophorylated
     gmax = p(16); % 0.14067
-    gmaxp = p(16)*(1.0-p(17)); % 0.14067*(1.0-0.387)
 
     act0 = 0.4139033547E-02;
     inact0 = 0.9999623535E+00;
-    actp0 = 0.8637307739E-03;
-    inactp0 = 0.9999881755E+00;
 
     % time space information
     t = time_space{1};
@@ -161,11 +158,7 @@ function [current_trc] = ikto(p, hold_volt, volt, time_space, Ek)
     act_hold = hh_model(hold_t, act0, gv_hold(1), gv_hold(3));
     inact_hold = hh_model(hold_t, inact0, gv_hold(2), gv_hold(4));      
 
-    actp_hold = hh_model(hold_t, actp0, gv_hold(5), gv_hold(7));
-    inactp_hold = hh_model(hold_t, inactp0, gv_hold(6), gv_hold(8));
-
-    current_trc(1:hold_idx) = (1-f_eacv).*gmax.*(act_hold.^3).*(inact_hold).*(hold_volt-Ek) + ...
-        f_eacv.*gmaxp.*(actp_hold.^3).*(inactp_hold).*(hold_volt-Ek);
+    current_trc(1:hold_idx) = gmax.*(act_hold.^3).*(inact_hold).*(hold_volt-Ek);
 
     % current equation at pulse voltage
     gv_pulse = ikto_gating_variables(p, volt);
@@ -173,59 +166,37 @@ function [current_trc] = ikto(p, hold_volt, volt, time_space, Ek)
     act_pulse = hh_model(pulse_t, act0, gv_pulse(1), gv_pulse(3));
     inact_pulse = hh_model(pulse_t, inact0, gv_pulse(2), gv_pulse(4));
 
-    actp_pulse = hh_model(pulse_t, actp0, gv_pulse(5), gv_pulse(7));
-    inactp_pulse = hh_model(pulse_t, inactp0, gv_pulse(6), gv_pulse(8));
-
-    current_trc((hold_idx+1):end) = (1-f_eacv).*gmax.*(act_pulse.^3).*(inact_pulse).*(volt-Ek) + ...
-        f_eacv.*gmaxp.*(actp_pulse.^3).*(inactp_pulse).*(volt-Ek);
+    current_trc((hold_idx+1):end) = gmax.*(act_pulse.^3).*(inact_pulse).*(volt-Ek);
 end
 
 function [gv] = ikto_gating_variables(p, V)
-    % p0 = [33, 15.5, 20, 16, 8, 7, 0.03577, 0.06237, 0.18064, 0.3956, ...
-    %     0.000152, 0.067083, 0.00095, 0.051335, 0.2087704319, 0.14067, 0.387];
+    % p0 = [33.0, 40.0, 33.0, 15.5, 20.0, 7.7, 5.7, ...
+    %     0.03577, 0.06237, 7.0, 0.18064, 0.3956, 0.0226585, 0.00095, 0.051335, 0.14067]
 
-    gv = zeros(8,1);
+    gv = zeros(4,1);
     
     % for Ikto
-    alpha1 = p(9).*exp(p(7).*(V+p(1)));
-    beta1 = p(10).*exp(-p(8).*(V+p(1)));
-    
-    alpha2_temp1 = p(11).*exp((V+p(2))./(-1.0.*p(6)));
-    alpha2_temp2 = p(12).*exp((V+p(2)+p(3))./(-1.0.*p(6)));
-    alpha2 = alpha2_temp1./(1.0+alpha2_temp2);
+    gv(1) = 1.0./(1.0+exp(-(p(1)+V)./p(6))); % ass
+    gv(2) = 1.0./(1.0+exp((p(2)+V)./p(7))); % iss
 
-    beta2_temp1 = p(13).*exp((V+p(2)+p(3))./p(6));
-    beta2_temp2 = p(14).*exp((V+p(2)+p(3))./p(6));
-    beta2 = beta2_temp1./(1.0+beta2_temp2);
-
-    gv(1) = alpha1./(alpha1+beta1);
-    gv(2) = alpha2./(alpha2+beta2);
+    alpha1 = p(11).*exp(p(8).*(V+p(3)));
+    beta1 = p(12).*exp(-p(9).*(V+p(3)));
     gv(3) = 1./(alpha1+beta1);
+    
+    alpha2_temp1 = exp((V+p(4))./(-1.0.*p(10)));
+    alpha2_temp2 = exp((V+p(4)+p(5))./(-1.0.*p(10)));
+    alpha2 = p(13).*(alpha2_temp1./alpha2_temp2);
+
+    beta2_temp1 = p(14).*exp((V+p(4)+p(5))./p(10));
+    beta2_temp2 = p(15).*exp((V+p(4)+p(5))./p(10));
+    beta2 = beta2_temp1./(beta2_temp2+1.0);
+
     gv(4) = 1./(alpha2+beta2);
-
-    % for Ikto phosphorylated
-    alpha1p = p(9).*exp(p(7).*(V+p(1)-p(4)));
-    beta1p = p(10).*exp(-p(8).*(V+p(1)-p(4)));
-    
-    alpha2_temp1p = p(11).*exp((V+p(2)-p(5))./(-1.0.*p(6)));
-    alpha2_temp2p = p(12).*exp((V+p(2)+p(3)-p(5))./(-1.0.*p(6)));
-    alpha2p = alpha2_temp1p./(1.0+alpha2_temp2p);
-    
-    beta2_temp1p = p(13).*exp((V+p(2)+p(3)-p(5))./p(6));
-    beta2_temp2p = p(14).*exp((V+p(2)+p(3)-p(5))./p(6));
-    beta2p = beta2_temp1p./(1.0+beta2_temp2p);
-
-    gv(5) = alpha1p./(alpha1p+beta1p);
-    gv(6) = alpha2p./(alpha2p+beta2p);
-    gv(7) = 1./(alpha1p+beta1p);
-    gv(8) = 1./(alpha2p+beta2p);
 end
 
 function [current_trc] = ikslow1(p, hold_volt, volt, time_space, Ek)
     % constants & initial values
-    f_eacv = p(11); % 0.9214774521 Ikslow1 fraction of nonphospholatedl
-    gmax = p(12); % 0.05766
-    gmaxp = p(13); % 0.07496
+    gmax = p(11); % 0.05766
     act0 = 0.5091689794e-03;
     inact0 = 0.9980927689;
     
@@ -241,13 +212,13 @@ function [current_trc] = ikslow1(p, hold_volt, volt, time_space, Ek)
     gv_hold = ikslow1_gating_variables(p, hold_volt);
     act_hold = hh_model(hold_t, act0, gv_hold(1), gv_hold(3));
     inact_hold = hh_model(hold_t, inact0, gv_hold(2), gv_hold(4));        
-    current_trc(1:hold_idx) = (gmax*f_eacv + gmaxp*(1-f_eacv)).*(act_hold).*(inact_hold).*(hold_volt - Ek);
+    current_trc(1:hold_idx) = gmax.*(act_hold).*(inact_hold).*(hold_volt - Ek);
 
     % current equation at pulse voltage
     gv_pulse = ikslow1_gating_variables(p, volt);
     act_pulse = hh_model(pulse_t, act0, gv_pulse(1), gv_pulse(3));
     inact_pulse = hh_model(pulse_t, inact0, gv_pulse(2), gv_pulse(4));
-    current_trc((hold_idx + 1):end) = (gmax*f_eacv + gmaxp*(1-f_eacv)).*(act_pulse).*(inact_pulse).*(volt - Ek);
+    current_trc((hold_idx + 1):end) = gmax.*(act_pulse).*(inact_pulse).*(volt - Ek);
 end
 
 function [gv] = ikslow1_gating_variables(p, V)
