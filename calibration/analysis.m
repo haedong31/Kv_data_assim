3e64d8f135cec572c8614b010f506a6f30362c50
Haedong Kim <haedong.kim90@gmail.com>
2021-09-13 08:05:42 -0400

    Fig 1 - Trace comparison

--- calibration/analysis.m
+++ calibration/analysis.m
@@ -3,74 +3,62 @@ clc
 close all 
 clear variables
 
-% specify result file to check
-file_group = 'ko';
-save_dir = 'calib_exp7';
-file_name = '15o27002.xlsx';
-calib = table2array(readtable(fullfile(pwd, strcat(save_dir, '_', file_group), file_name)));
+% specify result files and voltages to check
+file_group = 'wt';
+file_name = '15o26002.xlsx';
+
+save_dir1 = 'calib_exp11';
+save_dir2 = 'calib_exp14';
+
+% voltages info
+min_volt = -50;
+volt_range = 3:11;
+volts = NaN(length(volt_range), 1);
+for i = 1:length(volt_range)
+    volts(i) = min_volt + (volt_range(i)-1)*10;
+end
+
+% import calibration results
+calib1 = table2array(readtable(fullfile(pwd, strcat(save_dir1, '_', file_group), file_name)));
+calib2 = table2array(readtable(fullfile(pwd, strcat(save_dir2, '_', file_group), file_name)));
 
 % specify model structure
 % field 1: selection of currents
-current_names = {'ikto', 'ikslow1', 'ikslow2', 'ikss', 'ik1'};
+current_names = {'ikto', 'ikslow1', 'ikslow2', 'ikss'};
 num_currents = length(current_names);
 
 % field 2: tunning index in individual current models
-tune_idx1_kto = [1, 2, 3, 5, 6, 10, 13, 14, 15, 16, 17];
-tune_idx1_kslow1 = [1, 2, 3, 8, 9, 12, 13];
-tune_idx1_kslow2 = [1, 3];
-tune_idx1_kss = [3, 4];
-tune_idx1_kur = [1, 3];
-tune_idx1_k1 = [1, 3, 5, 7];
+tune_idx1 = cell(1, 6);
+tune_idx1{1} = [1, 2, 3, 5, 6, 10, 13, 14, 15, 16, 17];
+tune_idx1{2} = [1, 2, 3, 8, 9, 12, 13];
+tune_idx1{3} = [1, 3];
+tune_idx1{4} = [3, 4];
+tune_idx1{5} = [1, 3];
+tune_idx1{6} = [1, 3, 5, 7];
 
-idx_info1 = cell(1, num_currents);
-for i = 1:num_currents
-    switch current_names{i}
-    case "ikto"
-        idx_info1{i} = tune_idx1_kto;
-    case "ikslow1"
-        idx_info1{i} = tune_idx1_kslow1;
-    case "ikslow2"
-        idx_info1{i} = tune_idx1_kslow2;
-    case "ikss"
-        idx_info1{i} = tune_idx1_kss;
-    case "ikur"
-        idx_info1{i} = tune_idx1_kur;
-    case "ik1"
-        idx_info1{i} = tune_idx1_k1;
-    end
-end
+tune_idx2 = cell(1, 6);
+tune_idx2{1} = [1, 2, 6, 10, 13, 14, 15, 16, 17];
+tune_idx2{2} = [1, 2, 3, 4, 5, 8, 9, 11, 12, 13];
+tune_idx2{3} = [1, 3];
+tune_idx2{4} = [3, 4];
+tune_idx2{5} = [1, 3];
+tune_idx2{6} = [1, 3, 5, 7];
 
-% field 3: tunning index in decision variable, p
-idx_info2 = cell(1, num_currents);
-cul_idx_len = 0;
+[model_struct1, psize1] = gen_mdl_struct(current_names, tune_idx1);
+[model_struct2, psize2] = gen_mdl_struct(current_names, tune_idx2);
 
-for i = 1: num_currents
-    cul_idx_len_old = cul_idx_len;
-    cul_idx_len = cul_idx_len + length(idx_info1{i});
-    idx_info2{i} = (1+cul_idx_len_old):(cul_idx_len);
-end
-
-model_info = [current_names; idx_info1; idx_info2];
-field_names = {'name', 'idx1', 'idx2'};
-model_struct = cell2struct(model_info, field_names, 1);
-
-sol = zeros(cul_idx_len, 1);
-for i=1:num_currents
-    running_calib = calib(:, i);
-    running_calib(~isnan(running_calib));
-    sol(idx_info2{i}) = running_calib(idx_info1{i});
-end
+sol1 = gen_sol_vec(calib1, model_struct1, psize1);
+sol2 = gen_sol_vec(calib2, model_struct2, psize2);
 
 % experimental data to be compared
 exp_data = table2array(readtable(fullfile(pwd,'data', strcat(file_group, '-preprocessed2'), file_name)));
 t = exp_data(:, 1);
-yksum = exp_data(:, 2:end);
+yksum = exp_data(:, (volt_range+1));
 
 % protocol
 protocol = cell(4, 1);
 
 hold_volt = -70;
-volts = -50:10:50;
 ideal_hold_time = 120;
 ideal_end_time = 4.6*1000;
 ek = -91.1;
@@ -87,60 +75,82 @@ time_space{2} = t(1:ideal_hold_idx);
 pulse_t = t(ideal_hold_idx+1:end);
 pulse_t_adj = pulse_t - pulse_t(1);
 time_space{3} = pulse_t_adj;
-hold_idx = length(time_space{2});
+time_space{4} = ideal_hold_idx;
+time_space{5} = ideal_end_idx;
 
 protocol{1} = hold_volt;
 protocol{3} = time_space;
 protocol{4} = ek;
 
 % generate yksum_hat
+figure
 for i=1:length(volts)
     protocol{2} = volts(i);
     yksum_i = yksum(:, i);
-    [yksum_hat, comp] = kcurrent_model(sol, model_struct, protocol);
 
-    figure(i)
-    plot(t, yksum_i)
+    [yksum_hat1, ~] = kcurrent_model1(sol1, model_struct1, protocol);
+    [yksum_hat2, ~] = kcurrent_model1(sol2, model_struct2, protocol);
+ 
+    
+    subplot(3, 3, i)
+    plot(t, yksum_i, 'Color','red')
     hold on
-    plot(t, yksum_hat)
+    plot(t, yksum_hat1, '--', 'Color','green', 'LineWidth',2)
+    plot(t, yksum_hat2, ':', 'Color','blue', 'LineWidth',2)
     hold off
-    legend('Experimental','Model')
+    axis tight
+    grid on
+    legend('Experimental Data','Model Prediction 1', 'Model Prediction 2', 'Location','best')
+    title(strcat('Clamp Voltage: ', string(volts(i)), ' mV'), 'FontSize',10, 'FontWeight','bold');
+    xlabel('Time (ms)', 'FontSize',10, 'FontWeight','bold');
+    ylabel('Current (pA/pF)', 'FontSize',10, 'FontWeight','bold');
+    get(gcf,'CurrentAxes');
+    set(gca,'YDir','normal');
+    set(gca,'LineWidth',2, 'FontSize',10, 'FontWeight','bold');
+    set(gca,'GridLineStyle','--')
 end
 
-%% box plots of calibration parameters
-clc
-close all
-clear variables
-
-load('sols_ko.mat')
-load('sols_wt.mat')
-
-% remove rows with all zeros
-sols_wt = sols_wt(any(sols_wt, 2), :);
-sols_ko = sols_ko(any(sols_ko, 2), :);
-
-v = 3;
-g = 2;
-n = 33;
-
-boxplot_mat = zeros(v, g, n);
-
-for i = 1:v
-    for j = 1:g
-        for k = 1:n
-            if j == 1
-                boxplot_mat(i, j, k) = sols_wt(k, i);
-            else
-                boxplot_mat(i, j, k) = sols_ko(k, i);
-            end
+function [model_struct, psize] = gen_mdl_struct(current_names, tune_idx)
+    num_currents = length(current_names);
+    
+    % current model strcuture index 1
+    idx_info1 = cell(1, num_currents);
+    for i = 1:num_currents
+        switch current_names{i}
+        case 'ikto'
+            idx_info1{i} = tune_idx{1};
+        case 'ikslow1'
+            idx_info1{i} = tune_idx{2};
+        case 'ikslow2'
+            idx_info1{i} = tune_idx{3};
+        case 'ikss'
+            idx_info1{i} = tune_idx{4};
+        case 'ikur'
+            idx_info1{i} = tune_idx{5};
+        case 'ik1'
+            idx_info1{i} = tune_idx{6};
         end
     end
+
+    % current model strcuture index 2
+    idx_info2 = cell(1, num_currents);
+    psize = 0;
+    for i = 1: num_currents
+        psize_old = psize;
+        psize = psize + length(idx_info1{i});
+        idx_info2{i} = (1+psize_old):(psize);
+    end
+
+    model_info = [current_names; idx_info1; idx_info2];
+    field_names = {'name', 'idx1', 'idx2'};
+    model_struct = cell2struct(model_info, field_names, 1);
 end
 
-bp = boxplot2(boxplot_mat, 1:v);
-cmap = get(0, 'defaultaxescolororder');
-for ii = 1:2
-    structfun(@(x) set(x(ii,:), 'color', cmap(ii,:), ...
-        'markeredgecolor', cmap(ii,:)), bp);
+function sol = gen_sol_vec(sol_mx, model_struct, psize)
+    sol = zeros(1, psize);
+    for i = 1:length(model_struct)
+        running_sol = sol_mx(:, i);
+        running_sol = running_sol(~isnan(running_sol));
+        sol(model_struct(i).idx2) = running_sol(model_struct(i).idx1);
+    end
 end
-legend('WT','Mgat1KO', 'Location','best')
