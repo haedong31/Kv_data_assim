8e9cdbb5dc1a9fd973f7ee377d38b0d53f34c0af
Haedong Kim <haedong.kim90@gmail.com>
2022-01-01 18:03:11 -0500

    Parameter distribution visualization

--- calibration/param_dist_viz/param_dist.m
+++ calibration/param_dist_viz/param_dist.m
@@ -1,31 +1,13 @@
+%% visualize distributions of ikto parameters
 clc
 clearvars
 close all
 
-%% visualize distributions of ikto parameters
 pkto = readtable('pkto.csv');
 param = unique(pkto.param);
-psub = pkto(pkto.param==param(1),:);
-psub_wt = psub(string(psub.Group)=='WT',:);
-psub_ko = psub(string(psub.Group)=='Mgat1KO',:);
 
-[f1,xi1] = ksdensity(psub_wt.value);
-[f2,xi2] = ksdensity(psub_ko.value);
-
-figure('Color','w', 'Position',[100, 100, 600, 500])
-subplot(3,3,1)
-plot(xi1,f1, 'Color','blue', 'LineWidth',2)
-hold on
-plot(xi2,f2, 'Color','red', 'LineWidth',2)
-hold off
-axis tight
-xlabel(strcat('Parameter', num2str(param(1))))
-ylabel('Density')
-set(gca, 'LineWidth',2, 'FontName','Arial', 'FontSize',11, 'FontWeight','bold')
-legend('WT','Mgat1KO', 'Location','best')
-legend box off
-
-for i=2:length(param)
+figure('Color','w', 'Position',[100, 100, 600, 510])
+for i=1:length(param)
     psub = pkto(pkto.param==param(i),:);
     psub_wt = psub(string(psub.Group)=='WT',:);
     psub_ko = psub(string(psub.Group)=='Mgat1KO',:);
@@ -42,6 +24,108 @@ for i=2:length(param)
     xlabel(strcat('Parameter', num2str(param(i))))
     ylabel('Density')
     set(gca, 'LineWidth',2, 'FontName','Arial', 'FontSize',11, 'FontWeight','bold')
+
+    if i==4
+        legend('WT','Mgat1KO', 'Location','best')
+        legend box off
+    end
 end
 
 %% visualize distributions of ikslow1 parameters
+clc
+clearvars
+close all
+
+pkslow1 = readtable('pkslow1.csv');
+param = unique(pkslow1.param);
+
+figure('Color','w', 'Position',[100, 100, 600, 680])
+for i=1:length(param)
+    psub = pkslow1(pkslow1.param==param(i),:);
+    psub_wt = psub(string(psub.Group)=='WT',:);
+    psub_ko = psub(string(psub.Group)=='Mgat1KO',:);
+
+    [f1,xi1] = ksdensity(psub_wt.value);
+    [f2,xi2] = ksdensity(psub_ko.value);
+    
+    subplot(4,3,i)
+    plot(xi1,f1, 'Color','blue', 'LineWidth',2)
+    hold on
+    plot(xi2,f2, 'Color','red', 'LineWidth',2)
+    hold off
+    axis tight
+    xlabel(strcat('Parameter', num2str(param(i))))
+    ylabel('Density')
+    set(gca, 'LineWidth',2, 'FontName','Arial', 'FontSize',11, 'FontWeight','bold')
+
+    if i==4
+        legend('WT','Mgat1KO', 'Location','best')
+        legend box off
+    end
+end
+
+%% ikslow2
+clc
+clearvars
+close all
+
+pkslow2 = readtable('pkslow2.csv');
+param = unique(pkslow2.param);
+
+figure('Color','w', 'Position',[100, 100, 400, 170])
+for i=1:length(param)
+    psub = pkslow2(pkslow2.param==param(i),:);
+    psub_wt = psub(string(psub.Group)=='WT',:);
+    psub_ko = psub(string(psub.Group)=='Mgat1KO',:);
+
+    [f1,xi1] = ksdensity(psub_wt.value);
+    [f2,xi2] = ksdensity(psub_ko.value);
+    
+    subplot(1,2,i)
+    plot(xi1,f1, 'Color','blue', 'LineWidth',2)
+    hold on
+    plot(xi2,f2, 'Color','red', 'LineWidth',2)
+    hold off
+    axis tight
+    xlabel(strcat('Parameter', num2str(param(i))))
+    ylabel('Density')
+    set(gca, 'LineWidth',2, 'FontName','Arial', 'FontSize',11, 'FontWeight','bold')
+
+    if i==1
+        legend('WT','Mgat1KO', 'Location','best')
+        legend box off
+    end
+end
+
+%% ikss
+clc
+clearvars
+close all
+
+pkss = readtable('pkss.csv');
+param = unique(pkss.param);
+
+figure('Color','w', 'Position',[100, 100, 400, 170])
+for i=1:length(param)
+    psub = pkss(pkss.param==param(i),:);
+    psub_wt = psub(string(psub.Group)=='WT',:);
+    psub_ko = psub(string(psub.Group)=='Mgat1KO',:);
+
+    [f1,xi1] = ksdensity(psub_wt.value);
+    [f2,xi2] = ksdensity(psub_ko.value);
+    
+    subplot(1,2,i)
+    plot(xi1,f1, 'Color','blue', 'LineWidth',2)
+    hold on
+    plot(xi2,f2, 'Color','red', 'LineWidth',2)
+    hold off
+    axis tight
+    xlabel(strcat('Parameter', num2str(param(i))))
+    ylabel('Density')
+    set(gca, 'LineWidth',2, 'FontName','Arial', 'FontSize',11, 'FontWeight','bold')
+
+    if i==1
+        legend('WT','Mgat1KO', 'Location','best')
+        legend box off
+    end
+end
