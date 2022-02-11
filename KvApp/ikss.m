18e6ea5a9f91cb2a3a12fded920753989caa9733
Haedong Kim <haedong.kim90@gmail.com>
2022-02-10 22:31:12 -0500

    Currents without phosphorylation (KvApp)

--- KvApp/ikss.m
+++ KvApp/ikss.m
@@ -1,8 +1,6 @@
-function [current_trc] = ikss(p, hold_volt, volt, time_space, Ek)
-    % 7 parameters; {p(7): gmax}
-
+function current_trc = ikss(p, hold_volt, volt, time_space, ek)
     % constants & initial values
-    gmax = p(7); % 0.0428
+    gmax = p(7);
     act0 = 0.5091689794e-03;
 
     % time space information
@@ -14,25 +12,22 @@ function [current_trc] = ikss(p, hold_volt, volt, time_space, Ek)
     current_trc = zeros(length(t), 1);
 
     % current equation at holding
-    gv_hold = gating_variables(p, hold_volt);
-    act_hold = hh_model(hold_t, act0, gv_hold(1), gv_hold(2));
+    kv_hold = ikss_kinetic_variables(p, hold_volt);
+    act_hold = hh_model(hold_t, act0, kv_hold(1), kv_hold(2));
     current_trc(1:hold_idx) = gmax.*act_hold.*(hold_volt - Ek);
 
     % current equation at pulse voltage
-    gv_pulse = gating_variables(p, volt);
-    act_pulse = hh_model(pulse_t, act0, gv_pulse(1), gv_pulse(2));
+    kv_pulse = ikss_kinetic_variables(p, volt);
+    act_pulse = hh_model(pulse_t, act0, kv_pulse(1), kv_pulse(2));
     current_trc((hold_idx + 1):end) = gmax.*act_pulse.*(volt - Ek);
 end
 
-function [gv] = gating_variables(p, V)
-    % gv(1) = gv(1) in Ikslow1 = gv(1) in Ikslow2
-    % p0 = [22.5, 40.0, 7.7, 0.0862, 1235.5, 13.17, 0.0428]
-
-    gv = zeros(2,1);
-    gv(1) = 1.0./(1.0+exp(-(p(1)+V)./p(3)));
-    gv(2) = p(5)./(exp(p(4)*(V+p(2))) + exp(-p(4)*(V+p(2)))) + p(6);
+function kv = ikss_kinetic_variables(p, V)
+    kv = zeros(2,1);
+    kv(1) = 1.0./(1.0+exp(-(p(1)+V)./p(3)));
+    kv(2) = p(5)./(exp(p(4)*(V+p(2))) + exp(-p(4)*(V+p(2)))) + p(6);
 end
 
-function [y] = hh_model(t, ss0, ss, tau)
+function y = hh_model(t, ss0, ss, tau)
     y = ss - (ss - ss0).*exp(-t./tau);
 end
