2b277db4591b84cd6d499787688f9d4572a0a0cc
Haedong Kim <haedong.kim90@gmail.com>
2022-03-03 13:34:48 -0500

    Add IKtos to the app

---
+++ KvApp/iktos.m
@@ -0,0 +1,4 @@
+function [current_trc,kv_pulse] = iktos(p, hold_volt, volt, time_space, ek)
+    current_trc = 0;
+    kv_pulse = 0;
+end
---
+++ KvApp/run_calib.m
@@ -0,0 +1,3 @@
+function sol = run_calib(model_info,protocol_info,y)
+
+end
