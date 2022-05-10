39c8ed247eb2bfd8749ad01de789f172ac9cfaf2
Haedong Kim <haedong.kim90@gmail.com>
2022-05-10 13:23:22 -0400

    Add file names to data

--- calibration/param_dist_viz/param_preproc.py
+++ calibration/param_dist_viz/param_preproc.py
@@ -4,20 +4,33 @@ import pandas as pd
 import seaborn as sns
 
 def read_param_file(paths, currents, idx_dict):
-    plist = [list()]*len(currents)    
+    plist = []
+    for _ in range(len(currents)):
+        plist.append([])
+        
     for p in paths:
         param_df = pd.read_excel(p)
         for n,c in enumerate(currents):
-            plist[n].append(gen_param_dict(param_df[c], idx_dict[c]))
+            param_dict = gen_param_dict(p, param_df[c], idx_dict[c])
+            plist[n].append(param_dict)
 
     plist = [pd.DataFrame(x) for x in plist]
     return plist
 
-def gen_param_dict(p, idx):
+def gen_param_dict(p, pdf, idx):
     param_dict = {}
     for i in idx:
-        param_dict[str(i)] = p.iloc[i-1]
+        param_dict[str(i)] = pdf.iloc[i-1]
+    
+    p_wo_ext = p.with_suffix('')
+    param_dict['file'] = p_wo_ext.name
     return param_dict
+
+def melt_param(pdf_wt, pdf_ko):
+    pdf_wt['Group'] = 'WT'
+    pdf_ko['Group'] = 'Mgat1KO'
+    pdf = pd.concat([pdf_wt, pdf_ko], ignore_index=True)
+    return pdf.melt(id_vars=['Group','file'], var_name='param')
 
 def compare_param_dist(pdf_wt, pdf_ko, idx):
     pdf_wt['Group'] = 'WT'
@@ -26,12 +39,6 @@ def compare_param_dist(pdf_wt, pdf_ko, idx):
 
     p = sns.displot(pdf, x=str(idx), hue='Group', kind='kde', fill=True)
     p.set_xlabels('Parameter '+str(idx), fontsize=12)
-
-def melt_param(pdf_wt, pdf_ko):
-    pdf_wt['Group'] = 'WT'
-    pdf_ko['Group'] = 'Mgat1KO'
-    pdf = pd.concat([pdf_wt, pdf_ko], ignore_index=True)
-    return pdf.melt(id_vars='Group', var_name='param')
     
 exp_num =  '45'
 currents = ['iktof', 'ikslow1', 'ikslow2', 'ikss']
@@ -41,7 +48,7 @@ idx_dict = {'iktof': [1, 2, 4, 5, 7, 11, 13],
     'ikslow2': [2, 3],
     'ikss': [1, 2, 3, 4]}
 
-base_dir = Path('/Users/haedongkim/Documents/Kv_data_assim/calibration')
+base_dir = Path.cwd().parent
 wt_dir = base_dir / ('calib_exp'+exp_num+'_wt')
 ko_dir = base_dir / ('calib_exp'+exp_num+'_ko')
 fpaths_wt = wt_dir.glob('*.xlsx')
