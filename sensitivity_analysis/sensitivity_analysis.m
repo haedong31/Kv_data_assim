989bfe0ef6396de7b94b880237b711b7e7fb58c5
Haedong Kim <haedong.kim90@gmail.com>
2022-07-23 17:42:20 -0400

    Plot specifications

--- sensitivity_analysis/sensitivity_analysis.m
+++ sensitivity_analysis/sensitivity_analysis.m
@@ -745,224 +745,225 @@ dgn_kss = table2array(readtable('ikss_dgn.csv'));
 
 c = parula(5);
 
-fig = figure('Color','w','Position',[100,100,900,1200]);
+fig = figure('Color','w','Position',[100,100,500,680]);
 orient(fig,'landscape')
 
 % biomarker 1
 % kto
 subplot(3,2,1)
 [theta,idx,phiinv] = doe_plot(normalize(response_kto(:,1),'range'),dgn_kto);
-plot(phiinv,theta,'+','MarkerSize',10,'Color',c(1,:),'LineWidth',1.5)
+plot(phiinv,theta,'+','MarkerSize',8,'Color',c(1,:),'LineWidth',1.5)
 for i=1:length(theta)
-    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','bottom');
+    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','top');
 end
 
 hold on
 % kslow1
 [theta,idx,phiinv] = doe_plot(normalize(response_kslow1(:,1),'range'),dgn_kslow1);
-plot(phiinv,theta,'+','MarkerSize',10,'Color',c(2,:),'LineWidth',1.5)
+plot(phiinv,theta,'x','MarkerSize',8,'Color',c(2,:),'LineWidth',1.5)
 for i=1:length(theta)
-    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','bottom');
+    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','top');
 end
 
 % kslow2
 [theta,idx,phiinv] = doe_plot(normalize(response_kslow2(:,1),'range'),dgn_kslow2);
-plot(phiinv,theta,'+','MarkerSize',10,'Color',c(3,:),'LineWidth',1.5)
+plot(phiinv,theta,'*','MarkerSize',8,'Color',c(3,:),'LineWidth',1.5)
 for i=1:length(theta)
-    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','bottom');
+    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','top');
 end
 
 % kss
 [theta,idx,phiinv] = doe_plot(normalize(response_kss(:,1),'range'),dgn_kss);
-plot(phiinv,theta,'+','MarkerSize',10,'Color',c(4,:),'LineWidth',1.5)
+plot(phiinv,theta,'o','MarkerSize',8,'Color',c(4,:),'LineWidth',1.5)
 for i=1:length(theta)
-    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','bottom');
+    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','top');
 end
 hold off
-legend(["I_{Kto}","I_{Kslow1}","I_{Kslow2}","I_{Kss}"],'Location','northwest')
+legend(["I_{Kto}","I_{Kslow1}","I_{Kslow2}","I_{Kss}"],...
+    'Location','northeast','Box','off')
 title("Anchor Point 1")
 xlabel('Half-normal Quantiles')
-ylabel('Absolute Effects')
+ylabel('Normalized Absolute Effects')
 set(gca,'FontWeight','bold','LineWidth',1.5)
 
 % biomarker 2
 % kto
 subplot(3,2,2)
 [theta,idx,phiinv] = doe_plot(normalize(response_kto(:,2),'range'),dgn_kto);
-plot(phiinv,theta,'+','MarkerSize',10,'Color',c(1,:),'LineWidth',1.5)
+plot(phiinv,theta,'+','MarkerSize',8,'Color',c(1,:),'LineWidth',1.5)
 for i=1:length(theta)
-    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','bottom');
+    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','top');
 end
 
 hold on
 % kslow1
 [theta,idx,phiinv] = doe_plot(normalize(response_kslow1(:,2),'range'),dgn_kslow1);
-plot(phiinv,theta,'+','MarkerSize',10,'Color',c(2,:),'LineWidth',1.5)
+plot(phiinv,theta,'x','MarkerSize',8,'Color',c(2,:),'LineWidth',1.5)
 for i=1:length(theta)
-    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','bottom');
+    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','top');
 end
 
 % kslow2
 [theta,idx,phiinv] = doe_plot(normalize(response_kslow2(:,2),'range'),dgn_kslow2);
-plot(phiinv,theta,'+','MarkerSize',10,'Color',c(3,:),'LineWidth',1.5)
+plot(phiinv,theta,'*','MarkerSize',8,'Color',c(3,:),'LineWidth',1.5)
 for i=1:length(theta)
-    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','bottom');
+    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','top');
 end
 
 % kss
 [theta,idx,phiinv] = doe_plot(normalize(response_kss(:,2),'range'),dgn_kss);
-plot(phiinv,theta,'+','MarkerSize',10,'Color',c(4,:),'LineWidth',1.5)
+plot(phiinv,theta,'o','MarkerSize',8,'Color',c(4,:),'LineWidth',1.5)
 for i=1:length(theta)
-    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','bottom');
+    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','top');
 end
 hold off
 title("Anchor Point 2")
 xlabel('Half-normal Quantiles')
-ylabel('Absolute Effects')
+ylabel('Normalized Absolute Effects')
 set(gca,'FontWeight','bold','LineWidth',1.5)
 
 % biomarker 3
 % kto
 subplot(3,2,3)
 [theta,idx,phiinv] = doe_plot(normalize(response_kto(:,3),'range'),dgn_kto);
-plot(phiinv,theta,'+','MarkerSize',10,'Color',c(1,:),'LineWidth',1.5)
+plot(phiinv,theta,'+','MarkerSize',8,'Color',c(1,:),'LineWidth',1.5)
 for i=1:length(theta)
-    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','bottom');
+    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','top');
 end
 
 hold on
 % kslow1
 [theta,idx,phiinv] = doe_plot(normalize(response_kslow1(:,3),'range'),dgn_kslow1);
-plot(phiinv,theta,'+','MarkerSize',10,'Color',c(2,:),'LineWidth',1.5)
+plot(phiinv,theta,'x','MarkerSize',8,'Color',c(2,:),'LineWidth',1.5)
 for i=1:length(theta)
-    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','bottom');
+    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','top');
 end
 
 % kslow2
 [theta,idx,phiinv] = doe_plot(normalize(response_kslow2(:,3),'range'),dgn_kslow2);
-plot(phiinv,theta,'+','MarkerSize',10,'Color',c(3,:),'LineWidth',1.5)
+plot(phiinv,theta,'*','MarkerSize',8,'Color',c(3,:),'LineWidth',1.5)
 for i=1:length(theta)
-    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','bottom');
+    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','top');
 end
 
 % kss
 [theta,idx,phiinv] = doe_plot(normalize(response_kss(:,3),'range'),dgn_kss);
-plot(phiinv,theta,'+','MarkerSize',10,'Color',c(4,:),'LineWidth',1.5)
+plot(phiinv,theta,'o','MarkerSize',8,'Color',c(4,:),'LineWidth',1.5)
 for i=1:length(theta)
-    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','bottom');
+    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','top');
 end
 hold off
 title("Anchor Point 3")
 xlabel('Half-normal Quantiles')
-ylabel('Absolute Effects')
+ylabel('Normalized Absolute Effects')
 set(gca,'FontWeight','bold','LineWidth',1.5)
 
 % biomarker 4
 % kto
 subplot(3,2,4)
 [theta,idx,phiinv] = doe_plot(normalize(response_kto(:,4),'range'),dgn_kto);
-plot(phiinv,theta,'+','MarkerSize',10,'Color',c(1,:),'LineWidth',1.5)
+plot(phiinv,theta,'+','MarkerSize',8,'Color',c(1,:),'LineWidth',1.5)
 for i=1:length(theta)
-    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','bottom');
+    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','top');
 end
 
 hold on
 % kslow1
 [theta,idx,phiinv] = doe_plot(normalize(response_kslow1(:,4),'range'),dgn_kslow1);
-plot(phiinv,theta,'+','MarkerSize',10,'Color',c(2,:),'LineWidth',1.5)
+plot(phiinv,theta,'x','MarkerSize',8,'Color',c(2,:),'LineWidth',1.5)
 for i=1:length(theta)
-    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','bottom');
+    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','top');
 end
 
 % kslow2
 [theta,idx,phiinv] = doe_plot(normalize(response_kslow2(:,4),'range'),dgn_kslow2);
-plot(phiinv,theta,'+','MarkerSize',10,'Color',c(3,:),'LineWidth',1.5)
+plot(phiinv,theta,'*','MarkerSize',8,'Color',c(3,:),'LineWidth',1.5)
 for i=1:length(theta)
-    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','bottom');
+    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','top');
 end
 
 % kss
 [theta,idx,phiinv] = doe_plot(normalize(response_kss(:,4),'range'),dgn_kss);
-plot(phiinv,theta,'+','MarkerSize',10,'Color',c(4,:),'LineWidth',1.5)
+plot(phiinv,theta,'o','MarkerSize',8,'Color',c(4,:),'LineWidth',1.5)
 for i=1:length(theta)
-    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','bottom');
+    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','top');
 end
 hold off
 title("Anchor Point 4")
 xlabel('Half-normal Quantiles')
-ylabel('Absolute Effects')
+ylabel('Normalized Absolute Effects')
 set(gca,'FontWeight','bold','LineWidth',1.5)
 
 % biomarker 5
 % kto
 subplot(3,2,5)
 [theta,idx,phiinv] = doe_plot(normalize(response_kto(:,5),'range'),dgn_kto);
-plot(phiinv,theta,'+','MarkerSize',10,'Color',c(1,:),'LineWidth',1.5)
+plot(phiinv,theta,'+','MarkerSize',8,'Color',c(1,:),'LineWidth',1.5)
 for i=1:length(theta)
-    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','bottom');
+    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','top');
 end
 
 hold on
 % kslow1
 [theta,idx,phiinv] = doe_plot(normalize(response_kslow1(:,5),'range'),dgn_kslow1);
-plot(phiinv,theta,'+','MarkerSize',10,'Color',c(2,:),'LineWidth',1.5)
+plot(phiinv,theta,'x','MarkerSize',8,'Color',c(2,:),'LineWidth',1.5)
 for i=1:length(theta)
-    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','bottom');
+    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','top');
 end
 
 % kslow2
 [theta,idx,phiinv] = doe_plot(normalize(response_kslow2(:,5),'range'),dgn_kslow2);
-plot(phiinv,theta,'+','MarkerSize',10,'Color',c(3,:),'LineWidth',1.5)
+plot(phiinv,theta,'*','MarkerSize',8,'Color',c(3,:),'LineWidth',1.5)
 for i=1:length(theta)
-    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','bottom');
+    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','top');
 end
 
 % kss
 [theta,idx,phiinv] = doe_plot(normalize(response_kss(:,5),'range'),dgn_kss);
-plot(phiinv,theta,'+','MarkerSize',10,'Color',c(4,:),'LineWidth',1.5)
+plot(phiinv,theta,'o','MarkerSize',8,'Color',c(4,:),'LineWidth',1.5)
 for i=1:length(theta)
-    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','bottom');
+    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','top');
 end
 hold off
 title("Anchor Point 5")
 xlabel('Half-normal Quantiles')
-ylabel('Absolute Effects')
+ylabel('Normalized Absolute Effects')
 set(gca,'FontWeight','bold','LineWidth',1.5)
 
 % biomarker 6
 % kto
 subplot(3,2,6)
 [theta,idx,phiinv] = doe_plot(normalize(response_kto(:,6),'range'),dgn_kto);
-plot(phiinv,theta,'+','MarkerSize',10,'Color',c(1,:),'LineWidth',1.5)
+plot(phiinv,theta,'+','MarkerSize',8,'Color',c(1,:),'LineWidth',1.5)
 for i=1:length(theta)
-    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','bottom');
+    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','top');
 end
 
 hold on
 % kslow1
 [theta,idx,phiinv] = doe_plot(normalize(response_kslow1(:,6),'range'),dgn_kslow1);
-plot(phiinv,theta,'+','MarkerSize',10,'Color',c(2,:),'LineWidth',1.5)
+plot(phiinv,theta,'x','MarkerSize',8,'Color',c(2,:),'LineWidth',1.5)
 for i=1:length(theta)
-    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','bottom');
+    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','top');
 end
 
 % kslow2
 [theta,idx,phiinv] = doe_plot(normalize(response_kslow2(:,6),'range'),dgn_kslow2);
-plot(phiinv,theta,'+','MarkerSize',10,'Color',c(3,:),'LineWidth',1.5)
+plot(phiinv,theta,'*','MarkerSize',8,'Color',c(3,:),'LineWidth',1.5)
 for i=1:length(theta)
-    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','bottom');
+    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','top');
 end
 
 % kss
 [theta,idx,phiinv] = doe_plot(normalize(response_kss(:,6),'range'),dgn_kss);
-plot(phiinv,theta,'+','MarkerSize',10,'Color',c(4,:),'LineWidth',1.5)
+plot(phiinv,theta,'o','MarkerSize',8,'Color',c(4,:),'LineWidth',1.5)
 for i=1:length(theta)
-    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','bottom');
+    text(phiinv(i),theta(i),int2str(idx(i)),'HorizontalAlignment','right','VerticalAlignment','top');
 end
 hold off
 title("Anchor Point 6")
 xlabel('Half-normal Quantiles')
-ylabel('Absolute Effects')
+ylabel('Normalized Absolute Effects')
 set(gca,'FontWeight','bold','LineWidth',1.5)
 
 %% custrom functions
